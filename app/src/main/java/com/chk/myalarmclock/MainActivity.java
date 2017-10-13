768862b14c3921ecbf032aed6563c70df868093f
chenhk <chenhk@gionee.com>
2017-10-13 10:06:19 +0800

    first commit test
    Change-Id: I0ef4540e59ba8281a25717ca4927c9e73a332815

---
+++ app/src/main/java/com/chk/myalarmclock/MainActivity.java
@@ -0,0 +1,18 @@
+package com.chk.myalarmclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"测试中",Toast.LENGTH_SHORT).show();
+
+
+    }
+}
